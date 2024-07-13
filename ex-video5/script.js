64a79c9900f64badd92b4c25d27e9b8abb0346f5
Aniket Ghosh <121872510+Aniketghosh2003@users.noreply.github.com>
2024-07-13 17:57:27 +0530

    exercise-5

---
+++ ex-video5/script.js
@@ -0,0 +1,13 @@
+// const alphabet = (str) => 
+//     str.split('').sort((a,b) => a > b ? 1 :-1)
+//     .join('');
+
+// console.log(alphabet('sharmi'));    
+
+ 
+// const countvowels = (str ,letters = ['a','e','i','o','u']) =>
+//       str.split('').filter(s => letters.indexOf(s) > -1)
+//       .length;
+
+// console.log(countvowels('abc'));  
+
